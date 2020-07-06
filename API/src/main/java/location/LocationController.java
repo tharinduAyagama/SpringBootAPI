9588019a9052da6e3500dd36dd59834960f0efc7
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-06 21:38:59 +0530

    models controllers and services

---
+++ API/src/main/java/location/LocationController.java
@@ -0,0 +1,8 @@
+package location;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LocationController {
+
+}
---
+++ API/src/main/java/location/LocationService.java
@@ -0,0 +1,8 @@
+package location;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationService {
+
+}
