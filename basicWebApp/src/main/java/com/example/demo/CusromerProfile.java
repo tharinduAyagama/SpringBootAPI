3cbb9465ef2f72e2971b228bdb609f9adf0f471a
tharinduAyagama <tharinduhirantha32@gmail.com>
2020-07-08 00:55:00 +0530

    mode view

--- basicWebApp/src/main/java/com/example/demo/CusromerProfile.java
+++ basicWebApp/src/main/java/com/example/demo/CusromerProfile.java
@@ -2,11 +2,17 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class CusromerProfile {
 	@RequestMapping("/profile")
-	public String greet() {
-		return "profile";
+	public ModelAndView greet(@RequestParam("name") String name) {
+		
+		ModelAndView mv = new  ModelAndView();
+		mv.addObject("name" , name);
+		mv.setViewName("profile");
+		return mv;
 	}
 }
--- basicWebApp/src/main/webapp/customer/profile.jsp
+++ basicWebApp/src/main/webapp/customer/profile.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
- 	this is customer profile
+ 	this is customer profile ${name}
 </body>
 </html>
